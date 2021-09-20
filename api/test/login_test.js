68dc2eedf61aa1e5bdb0ce06c0e98912c1e7839a
Regi Saputra <regi.saputra@mola.tv>
2021-09-20 21:04:23 +0700

    Simple refactor code

--- api/test/login_test.js
+++ api/test/login_test.js
@@ -1,15 +1,15 @@
 const env = require('dotenv').config()
 const util = require('util')
 const chaiHttp = require('chai-http')
+
 const chai = require('chai')
-const expect = chai.expect
 chai.use(require('chai-json-schema'))
 chai.use(chaiHttp)
-const addContext = require('mochawesome/addContext')
 
-const page = require('../page/login_page.js');
+const expect = chai.expect
 
-var access_token;
+const { loginWithCredentials } = require('../common/apiRequest');
+
 describe('Login - [POST] /accounts/_/oauth2/v1/token', () => {
   const app_key = "wIHGzJhset"
   const grant_type = 'password' 
@@ -46,119 +46,112 @@ describe('Login - [POST] /accounts/_/oauth2/v1/token', () => {
     }
   }
 
-    afterEach(function(){
-      if (this.currentTest.state == 'failed') { 
-          console.log("    * Response Code: " + util.inspect(response.status,{depth: null, colors: true}) + "\n");
-          console.log("    * Response Header: " + util.inspect(response.header,{depth: null, colors: true}) + "\n");
-          console.log("    * Response body: " + util.inspect(response.body,{depth: null, colors: true}) + "\n");
-      }
-    })
-
-    it('Login Using Valid Email And Password', async() => {
-      const email = process.env.ACCOUNT_HBO_EMAIL
-      const password = process.env.ACCOUNT_HBO_PASSWORD
-
-      response =  await page.loginWithCredentials(app_key, grant_type, scope, email, password).then(res => res)
-
-      access_token = response.body.token_type+" "+response.body.access_token
-
-      expect(response.status).to.equal(200)
-      expect(response.body.access_token).to.not.null
-      expect(response.body.access_token).to.be.a("string")
-      expect(response.body.token_type).to.equal("Bearer")
-      expect(response.body.token_type).to.be.a("string")
-      expect(response.body.expires_in).to.not.null
-      expect(response.body.expires_in).to.be.a("number")
-      expect(response.body.refresh_token).to.not.null
-      expect(response.body.refresh_token).to.be.a("string")
-
-    }),
-
-    it('Login Using invalid Email And Valid Password', async() => {
-      const email = process.env.ACCOUNT_KAMAL_EMAIL
-      const password = process.env.ACCOUNT_KAMAL_PASSWORD
-      response =  await page.loginWithCredentials(app_key, grant_type, scope, email, invalid_password).then(res => res)
-      
-      expect(response.status).to.equal(400)
-      expect(response.body.status_code).to.equal(400)
-      expect(response.body.status_code).to.be.a("number")
-      expect(response.body.error).to.equal("invalid_id_or_password")
-      expect(response.body.error).to.be.a("string")
-      expect(response.body.error_code).to.equal("8010")
-      expect(response.body.error_code).to.be.a("string")
-      expect(response.body.error_description).to.equal("Either id or password you have entered is invalid")
-      expect(response.body.error_description).to.be.a("string")
-    }),
-
-    it('Login Using Valid Email And Invalid Password', async() => {
-      const email = process.env.ACCOUNT_CINCIN_EMAIL
-      const password = process.env.ACCOUNT_CINCIN_PASSWORD
-      response =  await page.loginWithCredentials(app_key, grant_type, scope, invalid_email, password).then(res => res)
-      
-      expect(response.status).to.equal(400)
-      expect(response.body.status_code).to.equal(400)
-      expect(response.body.status_code).to.be.a("number")
-      expect(response.body.error).to.equal("invalid_id_or_password")
-      expect(response.body.error).to.be.a("string")
-      expect(response.body.error_code).to.equal("8010")
-      expect(response.body.error_code).to.be.a("string")
-      expect(response.body.error_description).to.equal("Either id or password you have entered is invalid")
-      expect(response.body.error_description).to.be.a("string")
-    }),
-
-    it('Login Using invalid app_key', async() => {
-      
-      response =  await page.loginWithCredentials(invalid_app_key, grant_type, scope, email, password).then(res => res)
-      
-      expect(response.status).to.equal(400)
-      expect(response.body.status_code).to.equal(400)
-      expect(response.body.status_code).to.be.a("number")
-      expect(response.body.error).to.equal("invalid_client")
-      expect(response.body.error).to.be.a("string")
-      expect(response.body.error_code).to.equal("1120")
-      expect(response.body.error_code).to.be.a("string")
-      expect(response.body.error_description).to.equal("Client authentication failed (e.g., unknown client, no client authentication included, or unsupported authentication method)")
-      expect(response.body.error_description).to.be.a("string")
-    }),
-
-    it('Login Using invalid grand_type', async() => {
-      
-      response =  await page.loginWithCredentials(app_key, invalid_grand_type, scope, email, password).then(res => res)
-      
-      expect(response.status).to.equal(400)
-      expect(response.body.status_code).to.equal(400)
-      expect(response.body.status_code).to.be.a("number")
-      expect(response.body.error).to.equal("unsupported_grant_type")
-      expect(response.body.error).to.be.a("string")
-      expect(response.body.error_code).to.equal("1150")
-      expect(response.body.error_code).to.be.a("string")
-      expect(response.body.error_description).to.equal("The authorization grant type is not supported by the authorization server.")
-      expect(response.body.error_description).to.be.a("string")
-    }),
-
-    it('Login Using invalid scope', async() => {
-      
-      response =  await page.loginWithCredentials(app_key, grant_type, invalid_scope, email, password).then(res => res)
-      
-      expect(response.status).to.equal(400)
-      expect(response.body.status_code).to.equal(400)
-      expect(response.body.status_code).to.be.a("number")
-      expect(response.body.error).to.equal("invalid_scope")
-      expect(response.body.error).to.be.a("string")
-      expect(response.body.error_code).to.equal("1160")
-      expect(response.body.error_code).to.be.a("string")
-      expect(response.body.error_description).to.equal("The requested scope is invalid, unknown, malformed, or exceeds the scope granted by the resource owner.")
-      expect(response.body.error_description).to.be.a("string")
-    }),
-
-    it('Login Error Schema Validation', async() => {  
-      response =  await page.loginWithCredentials(app_key, grant_type, scope, email, invalid_password).then(res => res)
-      
-      expect(response.body).to.be.jsonSchema(loginErrorSchema)
-    })
-
+  afterEach(function(){
+    if (this.currentTest.state == 'failed') { 
+        console.log("    * Response Code: " + util.inspect(response.status,{depth: null, colors: true}) + "\n");
+        console.log("    * Response Header: " + util.inspect(response.header,{depth: null, colors: true}) + "\n");
+        console.log("    * Response body: " + util.inspect(response.body,{depth: null, colors: true}) + "\n");
+    }
   })
 
-  module.exports = {
-    access_token,
-}
+  it('Login Using Valid Email And Password', async() => {
+    const email = process.env.ACCOUNT_HBO_EMAIL
+    const password = process.env.ACCOUNT_HBO_PASSWORD
+
+    response =  await loginWithCredentials(app_key, grant_type, scope, email, password).then(res => res)      
+
+    expect(response.status).to.equal(200)
+    expect(response.body.access_token).to.not.null
+    expect(response.body.access_token).to.be.a("string")
+    expect(response.body.token_type).to.equal("Bearer")
+    expect(response.body.token_type).to.be.a("string")
+    expect(response.body.expires_in).to.not.null
+    expect(response.body.expires_in).to.be.a("number")
+    expect(response.body.refresh_token).to.not.null
+    expect(response.body.refresh_token).to.be.a("string")
+
+  }),
+
+  it('Login Using invalid Email And Valid Password', async() => {
+    const email = process.env.ACCOUNT_KAMAL_EMAIL
+    const password = process.env.ACCOUNT_KAMAL_PASSWORD
+    response =  await page.loginWithCredentials(app_key, grant_type, scope, email, invalid_password).then(res => res)
+    
+    expect(response.status).to.equal(400)
+    expect(response.body.status_code).to.equal(400)
+    expect(response.body.status_code).to.be.a("number")
+    expect(response.body.error).to.equal("invalid_id_or_password")
+    expect(response.body.error).to.be.a("string")
+    expect(response.body.error_code).to.equal("8010")
+    expect(response.body.error_code).to.be.a("string")
+    expect(response.body.error_description).to.equal("Either id or password you have entered is invalid")
+    expect(response.body.error_description).to.be.a("string")
+  }),
+
+  it('Login Using Valid Email And Invalid Password', async() => {
+    const email = process.env.ACCOUNT_CINCIN_EMAIL
+    const password = process.env.ACCOUNT_CINCIN_PASSWORD
+    response =  await page.loginWithCredentials(app_key, grant_type, scope, invalid_email, password).then(res => res)
+    
+    expect(response.status).to.equal(400)
+    expect(response.body.status_code).to.equal(400)
+    expect(response.body.status_code).to.be.a("number")
+    expect(response.body.error).to.equal("invalid_id_or_password")
+    expect(response.body.error).to.be.a("string")
+    expect(response.body.error_code).to.equal("8010")
+    expect(response.body.error_code).to.be.a("string")
+    expect(response.body.error_description).to.equal("Either id or password you have entered is invalid")
+    expect(response.body.error_description).to.be.a("string")
+  }),
+
+  it('Login Using invalid app_key', async() => {
+    
+    response =  await page.loginWithCredentials(invalid_app_key, grant_type, scope, email, password).then(res => res)
+    
+    expect(response.status).to.equal(400)
+    expect(response.body.status_code).to.equal(400)
+    expect(response.body.status_code).to.be.a("number")
+    expect(response.body.error).to.equal("invalid_client")
+    expect(response.body.error).to.be.a("string")
+    expect(response.body.error_code).to.equal("1120")
+    expect(response.body.error_code).to.be.a("string")
+    expect(response.body.error_description).to.equal("Client authentication failed (e.g., unknown client, no client authentication included, or unsupported authentication method)")
+    expect(response.body.error_description).to.be.a("string")
+  }),
+
+  it('Login Using invalid grand_type', async() => {
+    
+    response =  await page.loginWithCredentials(app_key, invalid_grand_type, scope, email, password).then(res => res)
+    
+    expect(response.status).to.equal(400)
+    expect(response.body.status_code).to.equal(400)
+    expect(response.body.status_code).to.be.a("number")
+    expect(response.body.error).to.equal("unsupported_grant_type")
+    expect(response.body.error).to.be.a("string")
+    expect(response.body.error_code).to.equal("1150")
+    expect(response.body.error_code).to.be.a("string")
+    expect(response.body.error_description).to.equal("The authorization grant type is not supported by the authorization server.")
+    expect(response.body.error_description).to.be.a("string")
+  }),
+
+  it('Login Using invalid scope', async() => {
+    
+    response =  await page.loginWithCredentials(app_key, grant_type, invalid_scope, email, password).then(res => res)
+    
+    expect(response.status).to.equal(400)
+    expect(response.body.status_code).to.equal(400)
+    expect(response.body.status_code).to.be.a("number")
+    expect(response.body.error).to.equal("invalid_scope")
+    expect(response.body.error).to.be.a("string")
+    expect(response.body.error_code).to.equal("1160")
+    expect(response.body.error_code).to.be.a("string")
+    expect(response.body.error_description).to.equal("The requested scope is invalid, unknown, malformed, or exceeds the scope granted by the resource owner.")
+    expect(response.body.error_description).to.be.a("string")
+  }),
+
+  it('Login Error Schema Validation', async() => {  
+    response =  await page.loginWithCredentials(app_key, grant_type, scope, email, invalid_password).then(res => res)
+    
+    expect(response.body).to.be.jsonSchema(loginErrorSchema)
+  })
+})
