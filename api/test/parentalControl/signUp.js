f9b35679120cdf20a68deb5062a318169bcf79dd
Cincin JS <cincinjs@gmail.com>
2021-10-08 13:35:20 +0700

    Update signUp.js

--- api/test/parentalControl/signUp.js
+++ api/test/parentalControl/signUp.js
@@ -10,31 +10,39 @@ const expect = chai.expect
 
 const { signUp } = require('../../common/apiRequest');
 const { getTokenFromFile } = require('../../common/getToken');
+const country = process.env.npm_config_country
 
 describe('Sign Up', () => {
   describe('Sign Up ', () => {
-    afterEach(function(){
-      if(process.env.DEBUG_MODE == 'true') {
-        if (this.currentTest.state == 'failed') { 
-          console.log("    * Request URL: " + util.inspect(response.request.url,{depth: null, colors: true}) + "\n");
-          console.log("    * Request Body: " + util.inspect(response.request._data,{depth: null, colors: true}) + "\n");
-          console.log("    * Response Code: " + util.inspect(response.status,{depth: null, colors: true}) + "\n");
-          console.log("    * Response Header: " + util.inspect(response.header,{depth: null, colors: true}) + "\n");
-          console.log("    * Response Body: " + util.inspect(response.body,{depth: null, colors: true}) + "\n");
+    afterEach(function () {
+      if (process.env.DEBUG_MODE) {
+        if (this.currentTest.state == 'failed') {
+          console.log("    * Request URL: " + util.inspect(response.request.url, { depth: null, colors: true }) + "\n");
+          console.log("    * Request Body: " + util.inspect(response.request._data, { depth: null, colors: true }) + "\n");
+          console.log("    * Response Code: " + util.inspect(response.status, { depth: null, colors: true }) + "\n");
+          console.log("    * Response Header: " + util.inspect(response.header, { depth: null, colors: true }) + "\n");
+          console.log("    * Response Body: " + util.inspect(response.body, { depth: null, colors: true }) + "\n");
         }
       }
     })
-  
-    it('User can register using valid email', async() => { 
-      const email =  "t.hbo+"+Number(new Date())+"@mola.tv"
-      const payload = 
-        {
-            "email"     : email,
-            "password"  : process.env.ACCOUNT_HBO_PASSWORD
-        }
-      
-      response =  await signUp(payload).then(res => res)
-      
+
+    it('User Register using legal age', async () => {
+      const email = "t.hbo+" + Number(new Date()) + "@mola.tv"
+      const payload =
+      {
+        "email": email,
+        "password": process.env.ACCOUNT_HBO_PASSWORD
+      }
+
+      if (country == 'sg' || country == 'my' || country == 'it' || country == 'gb') {
+        payload.birthdate = "1990-01-01"
+      }
+
+      if (process.env.DEBUG_MODE) {
+        console.log(payload)
+      }
+      response = await signUp(payload).then(res => res)
+
       expect(response.status).to.equal(200)
       expect(response.body).to.be.a('object')
       expect(response.body.data).to.be.a('object')
@@ -42,8 +50,57 @@ describe('Sign Up', () => {
       expect(response.body.data.method).to.equal('email')
       expect(response.body.data.sent_to).to.be.a('string')
       expect(response.body.data.sent_to).to.equal(email)
-      
-    })
-    
+    }),
+
+      it('User Register using birthdate', async () => {
+        const email = "t.hbo+" + Number(new Date()) + "@mola.tv"
+        const payload =
+        {
+          "email": email,
+          "password": process.env.ACCOUNT_HBO_PASSWORD,
+          "birthdate": "1995-01-01"
+
+        }
+
+        if (process.env.DEBUG_MODE) {
+          console.log(payload)
+        }
+        response = await signUp(payload).then(res => res)
+
+        expect(response.status).to.equal(200)
+        expect(response.body).to.be.a('object')
+        expect(response.body.data).to.be.a('object')
+        expect(response.body.data.method).to.be.a('string')
+        expect(response.body.data.method).to.equal('email')
+        expect(response.body.data.sent_to).to.be.a('string')
+        expect(response.body.data.sent_to).to.equal(email)
+      }),
+
+      it('User Register using non legal age', async () => {
+        const email = "t.hbo+" + Number(new Date()) + "@mola.tv"
+        const payload =
+        {
+          "email": email,
+          "password": process.env.ACCOUNT_HBO_PASSWORD
+        }
+
+        if (country == 'sg' || country == 'my' || country == 'it' || country == 'gb') {
+          payload.birthdate = "2010-01-01"
+        }
+
+        if (process.env.DEBUG_MODE) {
+          console.log(payload)
+        }
+        response = await signUp(payload).then(res => res)
+
+        expect(response.status).to.equal(200)
+        expect(response.body).to.be.a('object')
+        expect(response.body.data).to.be.a('object')
+        expect(response.body.data.method).to.be.a('string')
+        expect(response.body.data.method).to.equal('email')
+        expect(response.body.data.sent_to).to.be.a('string')
+        expect(response.body.data.sent_to).to.equal(email)
+      })
+
   })
 })
